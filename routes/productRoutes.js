8668c9837621348e301ad766f608960e7aa24f71
unknown <mimick.stern@gmail.com>
2023-05-04 21:09:59 +0300

    searchServer

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,38 +1,111 @@
-import express from 'express';
-import Product from '../models/productModel.js';
-
+import express from "express";
+import Product from "../models/productModel.js";
 
 const productRouter = express.Router();
 
-productRouter.get('/', async (req, res) => {
-    const products = await Product.find();
-    res.send(products);
+productRouter.get("/", async (req, res) => {
+  const products = await Product.find();
+  res.send(products);
 });
 
-productRouter.get('/categories', async (req, res) => {
-    const categories = await Product.find().distinct('category');
-    res.send(categories);
+const PAGE_SIZE = 2;
+
+productRouter.get("/search", async (req, res) => {
+  const { query } = req;
+  const pageSize = query.pageSize || PAGE_SIZE;
+  const page = query.page || 1;
+  const category = query.category || "";
+  const price = query.price || "";
+  const rating = query.rating || "";
+  const order = query.order || "";
+  const searchQuery = query.query || "";
+
+  const queryFilter =
+    searchQuery && searchQuery !== "all"
+      ? {
+          name: {
+            $regex: searchQuery,
+            $options: "i",
+          },
+        }
+      : {};
+  const categoryFilter = category && category !== "all" ? { category } : {};
+  const ratingFilter =
+    rating && rating !== "all"
+      ? {
+          rating: {
+            $gte: Number(rating),
+          },
+        }
+      : {};
+  const priceFilter =
+    price && price !== "all"
+      ? {
+          // 1-50
+          price: {
+            $gte: Number(price.split("-")[0]),
+            $lte: Number(price.split("-")[1]),
+          },
+        }
+      : {};
+  const sortOrder =
+    order === "featured"
+      ? { featured: -1 }
+      : order === "lowest"
+      ? { price: 1 }
+      : order === "highest"
+      ? { price: -1 }
+      : order === "toprated"
+      ? { rating: -1 }
+      : order === "newest"
+      ? { createdAt: -1 }
+      : { _id: -1 };
+
+  const products = await Product.find({
+    ...queryFilter,
+    ...categoryFilter,
+    ...priceFilter,
+    ...ratingFilter,
+  })
+    .sort(sortOrder)
+    .skip(pageSize * (page - 1))
+    .limit(pageSize);
+
+  const countProducts = await Product.countDocuments({
+    ...queryFilter,
+    ...categoryFilter,
+    ...priceFilter,
+    ...ratingFilter,
+  });
+  res.send({
+    products,
+    countProducts,
+    page,
+    pages: Math.ceil(countProducts / pageSize),
+  });
 });
 
-
-productRouter.get('/:slug', async (req,res) => {
-    const product = await Product.findOne({ slug: req.params.slug });
-    if(product) {
-        res.send(product);
-    } else {
-        res.status(404).send({message: 'Product not found'});
-    }
+productRouter.get("/categories", async (req, res) => {
+  const categories = await Product.find().distinct("category");
+  res.send(categories);
 });
 
-productRouter.get('/product/:id', async (req,res) => {
-    const product = await Product.findById(req.params.id);
-    if(product) {
-        res.send(product);
-    } else {
-        res.status(404).send({message: 'Product not found'});
-    }
+productRouter.get("/:slug", async (req, res) => {
+  const product = await Product.findOne({ slug: req.params.slug });
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product not found" });
+  }
 });
 
-
+productRouter.get("/product/:id", async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product not found" });
+  }
+});
 
 export default productRouter;
