b017e25ae98bf2283fd95020020a9c7ff99180b6
unknown <mimick.stern@gmail.com>
2023-05-28 21:04:51 +0300

    review

--- models/productModel.js
+++ models/productModel.js
@@ -1,5 +1,16 @@
 import mongoose from "mongoose";
 
+const reviewSchema = new mongoose.Schema(
+    {
+        username: { type: String, required: true },
+        comment: { type: String, required: true, maxLength: 400, minLength: 1 },
+        rating: { type: Number, required: true },
+    },
+    {
+        timestamps: true,
+      }
+   );
+
 const productSchema = new mongoose.Schema(
     {
         name: { type: String, required: true, unique: true },
@@ -13,6 +24,7 @@ const productSchema = new mongoose.Schema(
         countInStock: { type: Number, required: true },
         rating: { type: Number, required: true },
         numReviews: { type: Number, required: true },
+        reviews: [reviewSchema],
     },
     {
         timestamps: true,
